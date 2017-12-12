443bd6a1bad3490d277abe73b7034eac598b7aba
Jonathan Norton <Jmichaeln5@gmail.com>
2017-12-12 14:39:07 -0500

    first Heroku commit

---
+++ server.rb
@@ -0,0 +1,68 @@
+
+
+
+
+
+require 'sinatra'
+
+get '/' do
+  "Hello world"
+end
+
+
+
+# go to the command line and run  'curl http://localhost:4567' - makes http request
+
+
+
+
+
+##################
+#
+# get '/' do
+#   .. show something ..
+# end
+#
+# #
+#
+# post '/' do
+#   .. create something ..
+# end
+#
+# #
+#
+# put '/' do
+#   .. replace something ..
+# end
+#
+# #
+#
+# delete '/' do
+#   .. annihilate something ..
+# end
+#
+
+##################
+
+
+get '/' do
+	"Hello world"
+end
+
+get "/sinatra" do
+	"Hello Sinatra"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
