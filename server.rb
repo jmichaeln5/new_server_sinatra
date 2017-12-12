053f58dafcf1649a727934c0b450bab93d1d225b
Jonathan Norton <Jmichaeln5@gmail.com>
2017-12-12 18:09:42 -0500

    homework assignment-launching first web server with sinatra

--- server.rb
+++ server.rb
@@ -9,6 +9,11 @@ get '/' do
   "Hello world"
 end
 
+get "/sinatra" do
+	"Hello Sinatra"
+end
+
+
 
 
 # go to the command line and run  'curl http://localhost:4567' - makes http request
@@ -45,13 +50,6 @@ end
 ##################
 
 
-get '/' do
-	"Hello world"
-end
-
-get "/sinatra" do
-	"Hello Sinatra"
-end
 
 
 
